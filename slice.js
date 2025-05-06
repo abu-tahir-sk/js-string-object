3387bc4e2ec07f6425bf3e0f011a5bdb47652460
abu-tahir-sk <abutahir2626@gmail.com>
2025-05-05 20:24:53 -0900

    added slice

---
+++ slice.js
@@ -0,0 +1,14 @@
+const address = "andorkilla";
+const part = address.slice(2,5);
+console.log(part); 
+
+
+const sentence = 'I am a good and hardwrking person.';
+// console.log(sentence.split(' '))
+// console.log(sentence.split('a'));
+
+
+const realfrind = ['Rahim', 'kahim', 'lahim', 'fahim'];
+console.log(realfrind.join());
+console.log(realfrind.join('|'));
+console.log(realfrind.join('-'));
