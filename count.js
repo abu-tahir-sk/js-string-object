49686b4d08c41b675bf66f2875622ecc5816bbec
abu-tahir-sk <abutahir2626@gmail.com>
2025-05-06 20:31:31 -0900

    all puhs

---
+++ count.js
@@ -0,0 +1,13 @@
+// Count the number of properties.
+let student = {
+      name: 'Ariana Grande',
+      age: 21,
+      city: 'Gaibandha',
+      isStudent: true
+  };
+  const keys = Object.keys(student);
+  console.log("number:", keys.length);
+  console.log("property:",);
+  keys.forEach(key =>
+  {console.log(key);}
+  );
